1bc275c2eaa09dd3c4ffc4b494fb52492529b41a
liubr <1502473931@qq.com>
2023-05-13 19:37:00 +0800

    jxnews_hs

--- jxnews_hs/web/webX.go
+++ jxnews_hs/web/webX.go
@@ -7,9 +7,15 @@ import (
 	"time"
 )
 
+/*
+*
+https://wxc.jxnews.com.cn/Weixin/Answerjifa/sign_do.html
+?p=cH_App_zgwxRB_7F
+&name=%E9%AD%8F%E6%98%A5%E4%B8%BD&tel=18279973274&area=360302&institution=%E8%90%8D%E4%B9%A1%E5%AD%A6%E9%99%A2
+*/
 var t = "https://wxc.jxnews.com.cn/Weixin/Answerjifa/" // 地址
 // var cip = "222.104.171.128"                                                // ip
-var institution = "%E5%8D%97%E6%98%8C%E8%81%8C%E4%B8%9A%E5%A4%A7%E5%AD%A6" // 学校 // 答案
+// var institution = "%E5%8D%97%E6%98%8C%E8%81%8C%E4%B8%9A%E5%A4%A7%E5%AD%A6" // 学校 // 答案
 var getArr []mode.Tm
 
 func getAnswerArr() (answer string) {
@@ -26,8 +32,11 @@ func getAnswerArr() (answer string) {
 }
 
 // sign_do.html?p=cH_App_zgwxRB_7F&name=秦尘&tel=13576792042&area=360112&institution=江西软件职业技术大学
-// var urlGet = fmt.Sprint(t, "sign_do.html?p=cH_App_zgwxRB_7F&name=%E5%88%98%E7%82%B3%E8%8D%A3&tel=15970645022&cip=", cip, "&area=360112&institution=", institution)
-var urlGet = fmt.Sprint(t, "sign_do.html?p=cH_App_zgwxRB_7F&name=%E5%8F%B6%E5%9F%8E&tel=15970645399&area=360112&institution=", institution)
+var urlGet = fmt.Sprint(t, "sign_do.html?p=cH_App_zgwxRB_7F&name=秦尘&tel=13576792042&area=360112&institution=江西软件职业技术大学")
+
+//var urlGet = fmt.Sprint(t, "sign_do.html?p=cH_App_zgwxRB_7F&name=%E9%AD%8F%E6%98%A5%E4%B8%BD&tel=18279973274&area=360302&institution=%E8%90%8D%E4%B9%A1%E5%AD%A6%E9%99%A2")
+
+//var urlGet = fmt.Sprint(t, "sign_do.html?p=cH_App_zgwxRB_7F&name=%E5%88%98%E7%82%B3%E8%8D%A3&tel=15970645022&area=360112&institution=%E5%8D%97%E6%98%8C%E8%81%8C%E4%B8%9A%E5%A4%A7%E5%AD%A6")
 
 // tj_do.html?p=cH_App_zgwxRB_7F&answer=1%7C%7C1%7C%7C2%7C%7C2%7C%7C3%7C%7C4%7C%7C3%7C%7C4%7C%7C1%7C%7C4%7C%7C3%7C%7C4%7C%7C4%7C%7C4%7C%7C3%7C%7C3,4%7C%7C3,4%7C%7C3,4%7C%7C3,4%7C%7C3,4
 // var urlPost = fmt.Sprint(t, "tj_do.html?p=cH_App_zgwxRB_7F&cip=", cip, "&answer=", getAnswerArr())
@@ -39,7 +48,7 @@ func WebX() {
 
 	// 创建一个一个定时器
 	fmt.Println("定时器开始")
-	myT := time.NewTimer(30.6554 * 1000 * 1000 * 1000) // 设置7秒后执行一次
+	myT := time.NewTimer(18 * 1000 * 1000 * 1000) // 设置7秒后执行一次
 	<-myT.C
 	fmt.Println(" 30 s 时间到 ", time.Now().Unix())
 
